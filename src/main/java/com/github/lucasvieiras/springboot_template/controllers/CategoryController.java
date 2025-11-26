d447a94789d547ac43a77a7f42529715a67b2d2d
Lucas Vieira <lucasvieirasilva.lpta@gmail.com>
2025-11-26 01:00:33 -0300

    feat(dto/controllers): add CategoryDTO, factory, and controller endpoint
    Introduces CategoryDTO and CategoryDTOFactory for converting Category entities to DTOs. Implements a POST /category/create endpoint in CategoryController to handle category creation and return the created category as a DTO.

--- src/main/java/com/github/lucasvieiras/springboot_template/controllers/CategoryController.java
+++ src/main/java/com/github/lucasvieiras/springboot_template/controllers/CategoryController.java
@@ -1,3 +1,30 @@
 ﻿package com.github.lucasvieiras.springboot_template.controllers;
 
-public class CategoryController {}
+import com.github.lucasvieiras.springboot_template.dto.factories.CategoryDTOFactory;
+import com.github.lucasvieiras.springboot_template.dto.requests.CategoryDTO;
+import com.github.lucasvieiras.springboot_template.dto.requests.category.CategoryRequest;
+import com.github.lucasvieiras.springboot_template.entities.Category;
+import com.github.lucasvieiras.springboot_template.services.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+@AllArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final CategoryDTOFactory categoryDTOFactory;
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryRequest request) {
+        Category category = categoryService.createCategory(request);
+        CategoryDTO response = categoryDTOFactory.create(category);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/github/lucasvieiras/springboot_template/dto/factories/CategoryDTOFactory.java
@@ -0,0 +1,21 @@
+﻿package com.github.lucasvieiras.springboot_template.dto.factories;
+
+import com.github.lucasvieiras.springboot_template.dto.requests.CategoryDTO;
+import com.github.lucasvieiras.springboot_template.entities.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CategoryDTOFactory {
+    public CategoryDTO create(Category category) {
+        return new CategoryDTO(
+                category.getId(),
+                category.getValue()
+        );
+    }
+
+    public List<CategoryDTO> create(List<Category> categories) {
+        return categories.stream().map(this::create).toList();
+    }
+}
